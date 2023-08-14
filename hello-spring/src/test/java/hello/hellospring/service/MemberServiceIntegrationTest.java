9584f5bbf83eb49fc58a03e07b0b1f2eeca12960
chaen-ing <96906242+chaen-ing@users.noreply.github.com>
2023-08-14 20:53:15 +0900

    스프링 DB 접근 기술 : JDBC

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,65 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    // 필드 기반 Autowired로. 테스트니까 간단하게
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {   // 한글써도 상관없음
+        // given : 주어진 조건
+        Member member = new Member();
+        member.setName("spring");
+
+        // when : 검증할 것
+        Long saveId = memberService.join(member);
+
+        // then : 검증하기
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given : 이름이 중복되는 2명의 회원
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        /* 방법 1 : try, catch
+        try{
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+        */
+
+        // 방법 2 : 람다 로직 실행시 앞의 예외가 터져야함. 다른 예외를 넣는다면 예외가 터지지 않으므로 실패
+        // 메세지를 리턴함
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        //then
+    }
+}
