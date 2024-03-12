d7168c3f40062b3c5a2ed0d3ba3e36db8009f0e4
chaen-ing <kimce0405@naver.com>
2024-03-12 16:27:01 +0900

    주문과 할인 도메인

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member memeber = memberRepository.findById(memberId);   // 회원정보 조회
+        // 할인정책에 멤버 넘김 : 할인 정책 클래스에서 회원 등급에 따른 할인 금액을 계산해서 리턴해줌
+        int discountPrice = discountPolicy.discount(memeber, itemPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
