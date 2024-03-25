af0768e826b84cdf22321aea24275eeefdc42824
chaen-ing <kimce0405@naver.com>
2024-03-25 19:09:26 +0900

    탐색위치와 스캔대상

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // member패키지 하위 패키지 탐색
+        basePackages = "hello.core.member",
         // 설정 정보는 컴포넌트 스캔 대상에서 제외
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
