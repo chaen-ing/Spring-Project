f667b865bb7d1a6b50748a7cf900c841b8905301
chaen-ing <kimce0405@naver.com>
2024-05-27 22:05:39 +0900

    영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,16 +15,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAA");
 
-            for(Member member : result){
-                System.out.println("member = " + member.getName());
-            }
+            em.detach(member);
 
+            System.out.println("============");
             tx.commit();
         } catch (Exception e){
             tx.rollback();
