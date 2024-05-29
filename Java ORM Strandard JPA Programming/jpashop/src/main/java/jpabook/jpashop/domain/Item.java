c6ea10defe1f02679c64a128468c85bf3f3de40d
chaen-ing <kimce0405@naver.com>
2024-05-30 02:56:38 +0900

    실전 예제 3 - 다양한 연관관계 매핑

--- Java ORM Strandard JPA Programming/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ Java ORM Strandard JPA Programming/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,11 +1,11 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @Entity
@@ -20,5 +20,8 @@ public class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
