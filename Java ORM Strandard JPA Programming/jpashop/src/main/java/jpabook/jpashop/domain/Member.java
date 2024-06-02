1fc5ae18b0cbdcb5f168a7f079197b4598f28e07
chaen-ing <kimce0405@naver.com>
2024-06-03 00:25:25 +0900

    실전 예제 6 - 값 타입 매핑

--- Java ORM Strandard JPA Programming/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ Java ORM Strandard JPA Programming/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -16,14 +16,41 @@ public class Member extends BaseEntity{
 
     private String name;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
