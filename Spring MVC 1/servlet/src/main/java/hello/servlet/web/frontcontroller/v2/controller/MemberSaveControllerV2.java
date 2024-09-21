c23bba8e0bd34c549ef539c79c4bbef01df5358b
chaen-ing <kimce0405@naver.com>
2024-09-21 22:36:29 +0900

    코딩 컨벤션 + check style test

--- Spring MVC 1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ Spring MVC 1/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -1,29 +1,31 @@
 package hello.servlet.web.frontcontroller.v2.controller;
 
+import java.io.IOException;
+
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.Myview;
 import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
 public class MemberSaveControllerV2 implements ControllerV2 {
-    private MemberRepository memberRepository = MemberRepository.getInstance();
-    @Override
-    public Myview process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
+	private MemberRepository memberRepository = MemberRepository.getInstance();
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
+	@Override
+	public Myview process(HttpServletRequest request, HttpServletResponse response) throws
+		ServletException,
+		IOException {
+		String username = request.getParameter("username");
+		int age = Integer.parseInt(request.getParameter("age"));
 
-        // Model에 데이터를 보관
-        request.setAttribute("member", member);
+		Member member = new Member(username, age);
+		memberRepository.save(member);
 
-        return new Myview("/WEB-INF/views/save-result.jsp");
-    }
+		// Model에 데이터를 보관
+		request.setAttribute("member", member);
+
+		return new Myview("/WEB-INF/views/save-result.jsp");
+	}
 }
