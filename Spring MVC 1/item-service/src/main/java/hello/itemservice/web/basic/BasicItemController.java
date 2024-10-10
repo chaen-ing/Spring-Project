ac4b2966819c92fbb2057a869393f0f774b79aca
chaen-ing <kimce0405@naver.com>
2024-10-10 16:29:04 +0900

    - 상품 등록폼 - 상품 등록 처리 @ModelAttribute - 상품 수정

--- Spring MVC 1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ Spring MVC 1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -40,6 +43,71 @@ public class BasicItemController {
 		model.addAttribute("item", item);
 		return "basic/item";
 	}
+
+	// 상품 등록 폼 보기
+	@GetMapping("/add")
+	public String addForm() {
+		return "basic/addForm";
+	}
+
+	// 상품 등록 post
+	// @PostMapping("/add")
+	public String addItemV1(@RequestParam("itemName") String itemName,
+		@RequestParam("price") int price,
+		@RequestParam("quantity") Integer quantity,
+		Model model) {
+
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+
+		itemRepository.save(item);
+
+		model.addAttribute("item",item);
+
+		return "basic/addForm";
+	}
+
+
+	//@PostMapping("/add")
+	public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+		itemRepository.save(item);
+
+		// model.addAttribute("item",item);
+
+		return "basic/addForm";
+	}
+
+	// @PostMapping("/add")
+	public String addItemV3(@ModelAttribute Item item, Model model) {
+		// Item -> item으로 이름 변경해서 자동 저장
+
+		itemRepository.save(item);
+		return "basic/addForm";
+	}
+
+	@PostMapping("/add")
+	public String addItemV4(Item item, Model model) {
+		// @ModelAttribute도 생략
+		itemRepository.save(item);
+		return "basic/addForm";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable(name="itemId") Long itemId, Model model){
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item",item);
+		return "basic/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable(name="itemId") Long itemId, @ModelAttribute Item item){
+		itemRepository.update(itemId,item);
+		return "redirect:/basic/items/{itemId}";
+	}
+
+
 
 	// 테스트용 데이터
 	@PostConstruct
