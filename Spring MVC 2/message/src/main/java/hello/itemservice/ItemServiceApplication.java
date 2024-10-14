cb72a18fd30750fbb803bc887051887e2f3b0bb0
chaen-ing <kimce0405@naver.com>
2024-10-14 19:29:14 +0900

    스프링 메시지 소스 설정

--- Spring MVC 2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ Spring MVC 2/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,19 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+	/**
+	 * 메시지 소스 설정 직접 등록
+	 * @Bean
+	 * public MessageSource messageSource(){
+	 * 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	 * 		messageSource.setBasenames("messages", "errors");
+	 * 		messageSource.setDefaultEncoding("utf-8");
+	 * 		return messageSource;
+	 * }
+	 * MessageSource는 인터페이스 이므로 ResouceBundelMessageSource를 빈으로 등록
+	 * basenames : 설정 파일의 이름 지정 -> messages면 messages.properties찾아서 사용
+	 * 파일 위치는 /resouces//messages.properties
+	 */
 
 }
