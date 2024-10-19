cb124422c5582a72e9bbac19dfb6f019e2ce8681
chaen-ing <kimce0405@naver.com>
2024-10-19 17:00:49 +0900

    Validator 분리 2 - V6

--- Spring MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ Spring MVC 2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -11,7 +11,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +34,11 @@ public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
 	private final ItemValidator itemValidator;
+
+	@InitBinder
+	public void init(WebDataBinder dataBinder){
+		dataBinder.addValidators(itemValidator);
+	}
 
 	@GetMapping
 	public String items(Model model) {
@@ -216,7 +224,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+	//@PostMapping("/add")
 	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes, Model model) {
 
@@ -229,6 +237,23 @@ public class ValidationItemControllerV2 {
 
 		itemValidator.validate(item, bindingResult); //검증 로직으로 연결
 
+
+		//성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes, Model model) {
+		// 검증에 실패하면 다시 입력 폼으로
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
+			// 자동으로 넘어가니까 model에 담을 필요 x
+			return "validation/v2/addForm";
+		}
 
 		//성공 로직
 		Item savedItem = itemRepository.save(item);
