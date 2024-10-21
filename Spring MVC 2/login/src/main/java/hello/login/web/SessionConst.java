52f1511982d3ee73a335ca9c147981226e577af2
chaen-ing <kimce0405@naver.com>
2024-10-21 16:35:07 +0900

    서블릿 HTTP 세션1

---
+++ Spring MVC 2/login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,11 @@
+package hello.login.web;
+
+/**
+ * 상수 사용을 위해 만든 클래스 이므로 인터페이스나 추상클래스로 생성
+ * 	interface : 항상 public
+ * 	abstract class : public static final로 선언
+ */
+public interface SessionConst {
+	String LOGIN_MEMBER = "loginMember";
+
+}
